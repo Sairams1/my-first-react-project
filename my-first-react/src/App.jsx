c93f23b73b9bf324758acfa80fbfb3a989836b63
Venkata Sai Ram <sairamitha2002@gmail.com>
2026-02-05 13:02:50 +0530

    Update weather app logic, UI, and ignore env file

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from "react";
+import React, { useState } from "react"
 import "./styles/App.css";
 
-
 function App() {
-  const [mood, setMood] = useState("🙂 Neutral");
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const getWeather = async () => {
+    if (city === "") {
+      alert("Please enter a city name");
+      return;
+    }
+
+    try {
+      const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+      const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.error) {
+        setError("City not found");
+        setWeather(null);
+      } else {
+        setWeather(data);
+        setError("");
+      }
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  };
 
   return (
-    <div className="container">
-      <h1 className="title">Mood Changer 🎭</h1>
+    <div className="app">
+      <h1>🌦️ Weather App</h1>
 
-      <div className="mood-box">
-        <h2>Current Mood</h2>
-        <p className="mood">{mood}</p>
-      </div>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onFocus={() => {
+          setWeather(null);
+          setError("");
+  }}
+      />
 
-      <div className="buttons">
-        <button className="happy" onClick={() => setMood("😄 Happy")}>
-          Happy
-        </button>
+      <button onClick={getWeather}>Get Weather</button>
 
-        <button className="angry" onClick={() => setMood("😡 Angry")}>
-          Angry
-        </button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
 
-        <button className="sleepy" onClick={() => setMood("😴 Sleepy")}>
-          Sleepy
-        </button>
-      </div>
+      {weather && (
+        <div className="result">
+          <h2>{weather.location.name}</h2>
+          <p>Temperature: {weather.current.temp_c} °C</p>
+          <p>Condition: {weather.current.condition.text}</p>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
 
